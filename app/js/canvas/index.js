954b8182c3c260fde5a31c43bf982f4db49db466
gotpop <liamsobczak@gmail.com>
2017-06-02 14:38:00 +0100

    'commit'

--- app/js/canvas/index.js
+++ app/js/canvas/index.js
@@ -4,100 +4,94 @@ export default class Canvas {
     this.winWidth = window.innerWidth
     this.winHeight = window.innerHeight
     this.earth.src = "http://upload.wikimedia.org/wikipedia/commons/d/d2/Svg_example_square.svg"
+    // this.earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
     this.makeCanvas()
   }
   makeCanvas() {
 
-    var earth = new Image()
-    let winWidth = window.innerWidth
-    let winHeight = window.innerHeight
     let myCanvas = document.createElement("canvas")
-    myCanvas.width = winWidth
-    myCanvas.height = winHeight
+    myCanvas.width = this.winWidth
+    myCanvas.height = this.winHeight
     myCanvas.setAttribute('id', 'canvas')
     document.body.appendChild(myCanvas)
+
+
+
+
     var ctx = document.getElementById('canvas').getContext('2d')
-    //
+
+    console.log(this.earth);
+    // this.init()
+    this.addGradient(ctx)
+  }
+
+  init() {
+    // let drawStuff = this.draw()
+    // window.requestAnimationFrame(drawStuff);
+    this.draw()
+  }
+
+  draw() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+
+    ctx.globalCompositeOperation = 'destination-over';
+
+
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight); // clear canvas
+
+    ctx.save();
+    ctx.translate(150, 150);
+
+    // Earth
+    var time = new Date();
+    ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+    ctx.translate(105, 0);
+    ctx.fillRect(0, -12, 50, 24); // Shadow
+
+    let maEarth = this.earth
+
+    ctx.drawImage(maEarth, -12, -12);
+
+    ctx.restore();
+
+    ctx.beginPath();
+    ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+    ctx.stroke();
+
+  }
+
+  addGradient(ctx) {
+    // var ctx = document.getElementById('canvas').getContext('2d');
     // let radgrad = ctx.createRadialGradient(245, 245, 50, 252, 250, 800);
     // radgrad.addColorStop(0, '#A7D30C');
     // radgrad.addColorStop(0.9, '#019F62');
     // ctx.fillStyle = radgrad;
-    //
-    // ctx.drawImage(earth, 300, 300);
-    //
-    // function init() {
-    //   window.requestAnimationFrame(draw)
-    // }
-    //
-    // function draw() {
-    //
-    //   ctx.clearRect(0, 0, winWidth, winHeight) // clear canvas
-    //   ctx.save()
-    //   // ctx.translate(100, 300)
-    //
-    //   ctx.fillRect(0, 0, winWidth, winHeight);
-    //
-    //   var time = new Date()
-    //   ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 600) * time.getMilliseconds())
-    //   ctx.drawImage(earth, 300, 300);
-    //   ctx.restore();
-    //   window.requestAnimationFrame(draw)
-    // }
-    //
-    // init()
 
+    // use getContext to use the canvas for drawing
+               var ctx = canvas.getContext('2d');
 
+               // Create Linear Gradients
+               var lingrad = ctx.createLinearGradient(0,0,0,150);
 
-    var sun = new Image();
-    var moon = new Image();
-    var earth = new Image();
-    function init() {
-      sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
-      moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
-      earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
-      window.requestAnimationFrame(draw);
-    }
+               lingrad.addColorStop(0, '#00ABEB');
+               lingrad.addColorStop(0.5, '#fff');
 
-    function draw() {
-      var ctx = document.getElementById('canvas').getContext('2d');
+               lingrad.addColorStop(0.5, '#66CC00');
+               lingrad.addColorStop(1, '#fff');
 
-      ctx.globalCompositeOperation = 'destination-over';
-      ctx.clearRect(0, 0, 300, 300); // clear canvas
+               var lingrad2 = ctx.createLinearGradient(0,50,0,95);
+               lingrad2.addColorStop(0.5, '#000');
+               lingrad2.addColorStop(1, 'rgba(0,0,0,0)');
 
-      ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-      ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-      ctx.save();
-      ctx.translate(150, 150);
+               // assign gradients to fill and stroke styles
+               ctx.fillStyle = lingrad;
+               ctx.strokeStyle = lingrad2;
 
-      // Earth
-      var time = new Date();
-      ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-      ctx.translate(105, 0);
-      ctx.fillRect(0, -12, 50, 24); // Shadow
-      ctx.drawImage(earth, -12, -12);
-
-      // Moon
-      ctx.save();
-      ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-      ctx.translate(0, 28.5);
-      ctx.drawImage(moon, -3.5, -3.5);
-      ctx.restore();
-
-      ctx.restore();
-
-      ctx.beginPath();
-      ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-      ctx.stroke();
-
-      ctx.drawImage(sun, 0, 0, 300, 300);
-
-      window.requestAnimationFrame(draw);
-    }
-
-    init();
-
-
+               // draw shapes
+               ctx.fillRect(10,10,130,130);
+               ctx.strokeRect(50,50,50,50);
 
 
   }
+
 }
