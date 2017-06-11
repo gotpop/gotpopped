6f8caf3e13ebb6ef17e9abe18b67588eac01537d
gotpop <liamsobczak@gmail.com>
2017-06-11 19:00:23 +0100

    'commit'

--- app/components/home/index.js
+++ app/components/home/index.js
@@ -20,9 +20,15 @@ export default class Home {
 
     let maH1 = new Html({type: 'h1', text: this.pageObject.title}).build()
     let maP = new Html({type: 'p', text: this.pageObject.about}).build()
+    let makeA = new Html({
+      type: 'a',
+      href: '#work',
+      text: 'Veiw'
+    }).build()
 
     maSection.appendChild(maH1)
     maSection.appendChild(maP)
+    maSection.appendChild(makeA)
     maView.appendChild(maSection)
 
   }
--- app/components/social/index.js
+++ app/components/social/index.js
@@ -1,1 +1,26 @@
+/////////////////////////////////////////////////
+// Header
+/////////////////////////////////////////////////
+
+import Html from '../utils'
 import Social from './social.json'
+
+
+/////////////////////////////////////////////////
+// Social
+/////////////////////////////////////////////////
+
+export default class Social {
+  constructor() {
+    this.makeSocial()
+  }
+  makeSocial() {
+
+    let links = new Html({type: 'div', id: 'social', class: 'social'}).build()
+
+    siteTitleH1.appendChild(siteTitleH1Text)
+    header.appendChild(siteTitleH1)
+    document.body.appendChild(header)
+
+  }
+}
