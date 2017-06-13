e9ed89e7e00754afd785caaaa49b3b4405f68bc4
gotpop <liamsobczak@gmail.com>
2017-06-13 12:22:03 +0100

    'Brochureware'

--- app/components/header/index.js
+++ app/components/header/index.js
@@ -86,7 +86,9 @@ export default class Header {
     }
 
     makeLi.addEventListener('click', listenToThis)
-    makeLi.addEventListener('touchstart', listenToThis)
+    makeLi.addEventListener('touchstart', function(event) {
+      alert('Touch me!')
+    })
 
 
 
