794bfefcf223bea8d90faf0d69d77c9fdd08f1a4
gotpop <liamsobczak@gmail.com>
2017-06-16 20:37:05 +0100

    'Brochureware'

--- app/components/header/index.js
+++ app/components/header/index.js
@@ -10,12 +10,14 @@ import Icon from '../icons'
 import Html from '../utils'
 import SiteTitle from '../site-title'
 import MenuTrigger from '../menu-trigger'
+import Menu from '../menu'
 
 /////////////////////////////////////////////////
 // Header
 /////////////////////////////////////////////////
 
 export default class Header {
+
   constructor(siteStatus) {
     this.siteStatus = siteStatus
     this.makeHeader()
@@ -27,65 +29,14 @@ export default class Header {
     new SiteTitle(makeHeader)
     let MenuTriggerVar = new MenuTrigger(makeHeader)
 
-    let headerNav = new Html({type: 'nav', class: 'header-nav'}).build()
-    makeHeader.appendChild(headerNav)
-    let menuUl = new Html({type: 'ul', id: 'headerMenu', class: 'header-menu'}).build()
-    headerNav.appendChild(menuUl)
-    this.loopPagesData(menuUl)
+    new Menu(makeHeader)
+
 
     var makeWrap = document.getElementById('wrap-site');
     var theFirstChild = makeWrap.firstChild;
     makeWrap.insertBefore(makeHeader, theFirstChild);
 
     MenuTriggerVar.addListenerToMenuToggle()
-  }
-
-  loopPagesData(menuUl) {
-    for (let pageItem of PagesData) {
-      this.makeList(menuUl, pageItem)
-    }
-
-  }
-
-  makeList(menuUl, pageItem) {
-
-    let makeA = new Html({
-      type: 'a',
-      dataId: pageItem.pageId,
-      href: '#' + pageItem.pageId,
-      text: pageItem.name
-    }).build()
-
-    let makeLi = new Html({type: 'li', class: 'header-menu-list'}).build()
-
-    makeLi.appendChild(makeA)
-
-    function listenToThis(event) {
-      event.stopPropagation()
-      siteStatus.currentPage = makeA.getAttribute('data-id')
-      new MakePage
-
-      document.body.classList.remove('menu-open')
-
-      let thumbClassArray = document.getElementsByClassName('header-menu-list')
-
-      for (let i = 0; i < thumbClassArray.length; i++) {
-        thumbClassArray[i].classList.remove('active')
-        if (siteStatus.currentPage === makeA.getAttribute('data-id')) {
-          makeLi.classList.add('active')
-        }
-      }
-
-    }
-
-    makeLi.addEventListener('click', listenToThis)
-
-    menuUl.appendChild(makeLi)
-
-    if (siteStatus.currentPage === makeA.getAttribute('data-id')) {
-      makeLi.classList.add('active')
-    }
-
   }
 
 }
