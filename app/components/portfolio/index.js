21bd2d917bae32e67351bd0f3496c7025fbd5e9c
gotpop <liamsobczak@gmail.com>
2017-06-16 21:17:07 +0100

    'Brochureware'

--- app/components/portfolio/index.js
+++ app/components/portfolio/index.js
@@ -3,7 +3,7 @@
 /////////////////////////////////////////////////
 
 import PortfolioData from './portfolio.json'
-import MakePortfolioPage from './PortfolioItem'
+import MakePortfolioPage from './PortfolioItem.js'
 import {siteStatus} from '../site/status.js'
 import Html from '../utils'
 
