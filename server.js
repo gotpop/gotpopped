dbbfbc13f0f2a56715e38d61f1182dc87d457f60
Liam Sobczak <liam@gotpop.net>
2018-02-18 23:24:55 +0000

    Preflight for heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var express = require('express'),
   app = express(),
   http = require('http'),
+  bodyParser = require('body-parser'),
   httpServer = http.Server(app);
 
 // set the port of our application
@@ -12,11 +13,18 @@ var express = require('express'),
 var port = process.env.PORT || 9999;
 
 app.use(express.static(__dirname + '/dist'));
+app.use(bodyParser.text());
 
 app.get('*', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.post('/api/user', function (req, res) {
+  var body = req.body;
+  console.log(body)
+  res.send(req.body);
 });
 
 app.listen(port, function () {
-  console.log('GotPop is running on http://localhost:' + port);
+  console.log('GotPop members is running on http://localhost:' + port);
 });
