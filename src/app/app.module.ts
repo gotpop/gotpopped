ef49d8b901aac1cb4a5ec391b967a952c313a775
Liam Sobczak <liam@gotpop.net>
2018-02-19 12:38:40 +0000

    Reorganised

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,16 @@ import { routes } from "./app.routes";
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './shared/components/button/button.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './header/menu/menu.component';
-import { MenuTriggerComponent } from './header/menu-trigger/menu-trigger.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { SiteTitleComponent } from './header/site-title/site-title.component';
-import { SkillsComponent } from './skills/skills.component';
-import { SocialComponent } from './footer/social/social.component';
-import { PortfolioModule } from './portfolio/portfolio.module';
+import { FooterComponent } from './templates/footer/footer.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { HomeComponent } from './templates/home/home.component';
+import { MenuComponent } from './templates/header/menu/menu.component';
+import { MenuTriggerComponent } from './templates/header/menu-trigger/menu-trigger.component';
+import { PortfolioComponent } from './templates/portfolio/portfolio.component';
+import { SiteTitleComponent } from './templates/header/site-title/site-title.component';
+import { SkillsComponent } from './templates/skills/skills.component';
+import { SocialComponent } from './shared/components/social/social.component';
+import { PortfolioModule } from './templates/portfolio/portfolio.module';
 
 
 @NgModule({
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
-import { HomeComponent } from './home/home.component';
-import { PortfolioComponent } from "./portfolio/portfolio.component";
-import { SkillsComponent } from "./skills/skills.component";
+import { HomeComponent } from './templates/home/home.component';
+import { PortfolioComponent } from "./templates/portfolio/portfolio.component";
+import { SkillsComponent } from "./templates/skills/skills.component";
 
 export const router: Routes = [
     { path: '', component: HomeComponent },
