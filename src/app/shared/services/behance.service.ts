c4f40893a355fefff4001f919cc5ad769962e4f3
Liam Sobczak <liam@gotpop.net>
2018-02-19 21:05:30 +0000

    Updated BH

--- src/app/shared/services/behance.service.ts
+++ src/app/shared/services/behance.service.ts
@@ -13,6 +13,7 @@ export class BehanceService {
   private username = 'gotpop';
   private api_key = 'nMLvtkO7CdwZg0ld8S1lcWrLzRiW3nRQ';
   private key;
+  private projectKey;
 
   constructor(private _jsonp: Jsonp) { }
 
@@ -22,6 +23,13 @@ export class BehanceService {
       + this.api_key + '&callback=JSONP_CALLBACK';
 
     return this._jsonp.get(this.key).map(res => res.json());
+  }
+
+  getProject(project_id) {
+    this.projectKey = 'http://www.behance.net/v2/projects/' + project_id
+      + '?api_key=' + this.api_key + '&callback=JSONP_CALLBACK';
+
+    return this._jsonp.get(this.projectKey).map(res => res.json());
   }
 
 }
