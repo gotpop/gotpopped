893b413c0fee659685d808d29e80aa597f188b63
Liam Sobczak <liam@gotpop.net>
2018-02-19 14:07:33 +0000

    Work routing

--- src/app/templates/portfolio/portfolio-routing.module.ts
+++ src/app/templates/portfolio/portfolio-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WorkItemComponent } from './work-item/work-item.component';
 import { LandingComponent } from '../../portfolio/landing/landing.component';
+import { PortfolioComponent } from './portfolio.component';
 
 const routes: Routes = [
   { path: 'work', redirectTo: 'work/columbus', pathMatch: 'full' },
   {
-    path: 'work/columbus', component: LandingComponent,
+    path: 'work', component: PortfolioComponent,
     children: [
-      { path: 'work/columbus', component: WorkItemComponent },
+      { path: 'columbus', component: WorkItemComponent },
     ]
   }
 ];
