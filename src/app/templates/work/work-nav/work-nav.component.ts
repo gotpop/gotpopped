3f4515b251e20b01a4edb4f46addd0610de7db7c
Liam Sobczak <liam@gotpop.net>
2018-02-19 21:46:05 +0000

    Edit api hook and init GH

--- src/app/templates/work/work-nav/work-nav.component.ts
+++ src/app/templates/work/work-nav/work-nav.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { BehanceService } from '../../../shared/services/behance.service';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-work-nav',
   templateUrl: './work-nav.component.html',
   styleUrls: ['./work-nav.component.scss']
 })
+
 export class WorkNavComponent implements OnInit {
 
-  constructor() { }
+  public projects: any;
+
+  constructor(private _behanceService: BehanceService) { }
 
   ngOnInit() {
+    this._behanceService.getProjects().subscribe(projects => {
+      this.projects = projects.projects;
+      console.log(this.projects, 'this.projects');
+    });
   }
 
 }
