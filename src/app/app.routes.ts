d82e71b6290056172626acae9649db14a5beb254
Liam Sobczak <liam@gotpop.net>
2018-02-22 01:11:29 +0000

    Fixed menu layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,14 @@ import { SkillsComponent } from './templates/skills/skills.component';
 
 export const router: Routes = [
   { path: '', component: HomeComponent },
+  {
+    path: 'work',
+    redirectTo: '/work/62283705',
+    pathMatch: 'full'
+  },
   { path: 'work/:id', component: WorkComponent },
   { path: 'skills', component: SkillsComponent },
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
+
