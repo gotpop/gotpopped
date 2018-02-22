db1efda40ba3cffc09078fa38808f502fc98ea9b
Liam Sobczak <liam@gotpop.net>
2018-02-22 16:55:46 +0000

    Updated colour vars

--- src/app/templates/work/work-nav/work-nav.component.ts
+++ src/app/templates/work/work-nav/work-nav.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { BehanceService } from '../../../shared/services/behance.service';
+import { trigger, transition, style, animate, query, stagger, state } from '@angular/animations';
 
 @Component({
   selector: 'app-work-nav',
   templateUrl: './work-nav.component.html',
-  styleUrls: ['./work-nav.component.scss']
+  styleUrls: ['./work-nav.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [ // each time the binding value changes
+        query(':enter', [
+          style({ opacity: 0 }),
+          stagger(100, [
+            animate('.5s', style({ opacity: 1 }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 
 export class WorkNavComponent implements OnInit {
@@ -16,7 +29,6 @@ export class WorkNavComponent implements OnInit {
   ngOnInit() {
     this._behanceService.getProjects().subscribe(projects => {
       this.projects = projects.projects;
-      console.log(this.projects, 'this.projects nav');
     });
   }
 
