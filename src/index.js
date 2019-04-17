c771176494c62997524d2c46180663966b332087
gotpop <liam@gotpop.net>
2019-04-17 09:27:35 +0100

    Fixed up basic build p;rocess and added back scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/index.scss';
-import './styles/scss/App.scss';
+import './assets/scss/index.scss';
+// import './styles/scss/App.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
