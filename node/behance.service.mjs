3e66da6b388a4b87eaec48629225c9b506078842
gotpop <liam@gotpop.net>
2019-04-21 17:41:29 +0100

    Adding Behance service

---
+++ node/behance.service.mjs
@@ -0,0 +1,28 @@
+/* Behance service */
+class BehanceService {
+
+ constructor(username, api_key) {
+    this.username = username;
+    this.api_key = api_key;
+    this.projectsApiUrl;
+    this.projectApiUrl;
+  }
+
+  getProjects() {
+   this.projectsApiUrl = 'https://www.behance.net/v2/users/'
+     + this.username + '/projects?api_key='
+     + this.api_key;
+
+   return this.projectsApiUrl;
+ }
+
+  getProject(project_id) {
+   this.projectApiUrl = 'https://www.behance.net/v2/projects/' + project_id
+     + '?api_key=' + this.api_key;
+
+   return this._jsonp.get(this.projectApiUrl).map(response => response.json());
+ }
+
+}
+
+export default BehanceService;
--- server.mjs
+++ server.mjs
@@ -1,80 +1,38 @@
 //server.js
-import Behance from "node-behance-api";
 import dotenv from 'dotenv';
 dotenv.config();
-
-// https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-when-using-the-experimental-modules-flag
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-// console.log('Hello', process.env);
-
-import express from 'express';
-// import favicon from 'express-favicon';
 import path from 'path';
+import express from 'express';
+import Behance from './node/behance.service'
+
+// Node 10+ does not support __dirname
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+//Express
 const port = process.env.PORT || 8080;
 const app = express();
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
+
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.post('/api/user', function (req, res) {
+  var body = req.body;
+  console.log(body)
+  res.send(req.body);
 });
 
 app.listen(port);
 
 
-// var Behance = require("node-behance-api");
-// var behance = new Behance({"client_id": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"})
-// Behance.initOptions();
-// behance.get({
-//     api: Behance.APIS.GET_USER,
-//     params: { //or simply behance.get('user',
-//         user:'deepakmshrma'
+let goBehance = new Behance(process.env.BE_USERNAME, process.env.BE_API_KEY);
 
-//     }
-// }, function (error, result) {
-//     if (error)
-//         console.log(error)
-//     else
-//         console.log(result)
-// });
+let thisBehance = goBehance.getProjects();
 
-
-
-
-
-
-
-// //////////////////////////////////////////////////////////////////////
-// // Note : Express is only used on Heroku
-// //////////////////////////////////////////////////////////////////////
-
-// var express = require('express'),
-//   app = express(),
-//   http = require('http'),
-//   bodyParser = require('body-parser'),
-//   httpServer = http.Server(app);
-
-// // set the port of our application
-// // process.env.PORT lets the port be set by Heroku
-// var port = process.env.PORT || 9999;
-
-// app.use(express.static(__dirname + '/dist'));
-// app.use(bodyParser.text());
-
-// app.get('*', function (req, res) {
-//   res.sendFile(__dirname + '/dist/index.html');
-// });
-
-// app.post('/api/user', function (req, res) {
-//   var body = req.body;
-//   console.log(body)
-//   res.send(req.body);
-// });
-
-// app.listen(port, function () {
-//   console.log('GotPop members is running on http://localhost:' + port);
-// });
+console.log('Hello', thisBehance);
