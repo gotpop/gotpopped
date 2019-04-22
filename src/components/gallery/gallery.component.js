56ab879c0f9689605e89e1e7df04b63c7222cf5d
gotpop <liam@gotpop.net>
2019-04-22 22:10:57 +0100

    Carousel works

--- src/components/gallery/gallery.component.js
+++ src/components/gallery/gallery.component.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Glide from '@glidejs/glide';
 import "./gallery.component.scss";
+
+// import moduleName from 'module';
 
 class Gallery extends Component {
 
@@ -15,42 +18,47 @@ class Gallery extends Component {
         this.setState({ allProjectsArray: data.projects });
         this.getProject(data.projects);
       });
-  }
-
-  getProject(projects) {
-    let singleProjects = [];
-    let promiseArray = [];
-    
-    // Create an array of fetch promises 
-    for (const project of projects) {
-      promiseArray.push(
-        fetch(`/behance/project/?projectId=${project.id}`)
-        .then(response => response.json())
-        .then(data => {
-          singleProjects.push(data.project);
-        })
-      );
+      
+      
+      
     }
-
-    // Execute and wait for all to resolve before setting state
-    Promise.all(promiseArray).then(()=> {
-      this.setState({ singleProjectsArray: singleProjects });
+    
+    getProject(projects) {
+      let singleProjects = [];
+      let promiseArray = [];
+      
+      // Create an array of fetch promises 
+      for (const project of projects) {
+        promiseArray.push(
+          fetch(`/behance/project/?projectId=${project.id}`)
+          .then(response => response.json())
+          .then(data => {
+            singleProjects.push(data.project);
+          })
+          );
+        }
+        
+        // Execute and wait for all to resolve before setting state
+        Promise.all(promiseArray).then(()=> {
+          this.setState({ singleProjectsArray: singleProjects });
+          new Glide('.glide').mount();
     });
   }
 
   render() {
     return (
-      <div>
-        <h1>Work</h1>
-        <ul className="gp-gallery">
-          {this.state.singleProjectsArray.map(movie => {
-            return <li key={`movie-${movie.id}`} className={movie.name}>
-            <h1>{movie.name}</h1>
-            <p>{movie.id}</p>
-            <img className={movie.name} key={movie.id} src={movie.modules[0].sizes.original} alt="Gallery"/>
-            </li>
-          })}
-        </ul>
+      <div className="glide">
+        <div className="glide__track" data-glide-el="track">
+          <ul className="gp-gallery glide__slides">
+            {this.state.singleProjectsArray.map(movie => {
+              return <li key={`movie-${movie.id}`} className="glide__slide">
+              <h1>{movie.name}</h1>
+              <p>{movie.id}</p>
+              <img className={movie.name} key={movie.id} src={movie.modules[0].sizes.original} alt="Gallery"/>
+              </li>
+            })}
+          </ul>
+        </div>
       </div>
     )
   }
