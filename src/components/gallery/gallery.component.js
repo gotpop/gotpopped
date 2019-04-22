4ed3c207669d4480f1b1cb4a4949f2d52b44232c
gotpop <liam@gotpop.net>
2019-04-22 21:39:07 +0100

    Comments and tidy up

--- src/components/gallery/gallery.component.js
+++ src/components/gallery/gallery.component.js
@@ -5,14 +5,14 @@ class Gallery extends Component {
 
   state = {
     singleProjectsArray : [],
-    movies : []
+    allProjectsArray : []
   };
 
   componentDidMount() {
     fetch('/behance/projects')
       .then(response => response.json())
       .then(data => {
-        this.setState({ movies: data.projects });
+        this.setState({ allProjectsArray: data.projects });
         this.getProject(data.projects);
       });
   }
@@ -21,25 +21,21 @@ class Gallery extends Component {
     let singleProjects = [];
     let promiseArray = [];
     
+    // Create an array of fetch promises 
     for (const project of projects) {
       promiseArray.push(
         fetch(`/behance/project/?projectId=${project.id}`)
         .then(response => response.json())
         .then(data => {
           singleProjects.push(data.project);
-          // console.log('Log name: ', singleProjects);
         })
-
       );
-      
     }
 
+    // Execute and wait for all to resolve before setting state
     Promise.all(promiseArray).then(()=> {
       this.setState({ singleProjectsArray: singleProjects });
     });
-
-
-    
   }
 
   render() {
