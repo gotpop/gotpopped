7cac89dcc9c6cdb6bf64235a24679023cf9d7def
gotpop <liam@gotpop.net>
2019-04-27 18:30:53 +0100

    Fixed work markup structure

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -6,7 +6,7 @@ class HomeComponent extends Component {
 
   render() {
     return (
-      <section className="si__view si__view--home">
+      <section className="ma__view ma__view--home">
         <h2>Hello, my name's Liam.</h2>
         <p>I build enterprise websites that perform well on all devices, focusing on responsive design, aesthetics, clean code &amp;
           page speed.</p>
--- src/components/work/work.component.js
+++ src/components/work/work.component.js
@@ -5,7 +5,9 @@ import Gallery from '../gallery/gallery.component';
 class WorkComponent extends Component {
   render() {
     return (
-      <Gallery></Gallery>
+      <section className="ma__view ma__view--work">
+        <Gallery></Gallery>
+      </section>
     );
   }
 }
