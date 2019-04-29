eff7f13ea40c70d4c47a2c11b908cc65cb4976c2
gotpop <liam@gotpop.net>
2019-04-29 18:23:31 +0100

    Added clients data and animations

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,15 +1,12 @@
 /* Skills data */
-const skillsObject = [
-  { name: 'Html 5' },
-  { name: 'CSS3' },
-  { name: 'Vannila JavaScript ES6' },
-  { name: 'Single page applications' },
-  { name: 'Rich media web' },
-  { name: 'Git' },
-  { name: 'Webpack' },
-  { name: 'UI Design' },
-  { name: 'Angular(2)' },
-  { name: 'OO Javascript' }
+const clientsObject = [
+  { name: 'Domestos' },
+  { name: 'Mercedes Benz' },
+  { name: 'Aetna' },
+  { name: 'Columbus' },
+  { name: 'Magnum' },
+  { name: 'Discovery' },
+  { name: 'Virgin Radio' }
 ];
 
-export default skillsObject;
+export default clientsObject;
