2d67d0214529c1feb2a55c34df33d30b1eeb8689
gotpop <liam@gotpop.net>
2019-05-05 13:39:51 +0100

    Formatting

--- server.mjs
+++ server.mjs
@@ -9,7 +9,7 @@ import fetch from "node-fetch";
 
 // Node 10+ does not support __dirname
 const __dirname = path.dirname(new URL(
-  import.meta.url).pathname);
+    import.meta.url).pathname);
 
 //Express variables
 const port = process.env.PORT || 3001;
@@ -21,48 +21,48 @@ const goBehance = new Behance(process.env);
 const thisBehance = goBehance.getProjects();
 
 // Force https unless local dev environment
-app.use(function(req, res, next) {
-  if ((req.get('X-Forwarded-Proto') !== 'https') && envy !== 'local') {
-    res.redirect('https://' + req.get('Host') + req.url);
-  } else
-    next();
+app.use(function (req, res, next) {
+    if ((req.get('X-Forwarded-Proto') !== 'https') && envy !== 'local') {
+        res.redirect('https://' + req.get('Host') + req.url);
+    } else
+        next();
 });
 
-// app.use(express.static(path.join(__dirname1, 'build')));
-
-app.use(express.static('build', {extensions: ['html', 'htm']}));
-// app.use(express.static('index.html', { root: './build' }));
-
+// Set up server
+app.use(express.static('build', {
+    extensions: ['html', 'htm']
+}));
 app.use(favicon(__dirname + '/public/favicon.png'));
 
+// Api
 app.get('/behance/projects', function (req, res) {
-  fetch(thisBehance)
-  .then(response => response.json())
-  .then(data => {
-    res.send(data);
-  })
-  .catch(error => console.error(error));
+    fetch(thisBehance)
+        .then(response => response.json())
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => console.error(error));
 });
 
 app.get('/behance/project', function (req, res) {
+    let projectId = req.query.projectId;
+    const thisBehanceProject = goBehance.getProject(projectId);
+    console.log("thisBehance", thisBehanceProject);
 
-  let projectId = req.query.projectId;
-  const thisBehanceProject = goBehance.getProject(projectId);
-  console.log("thisBehance", thisBehanceProject);
-  
-  fetch(thisBehanceProject)
-  .then(response => response.json())
-  .then(data => {
-    res.send(data);
-  })
-  .catch(error => console.error(error));
+    fetch(thisBehanceProject)
+        .then(response => response.json())
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => console.error(error));
 });
 
 app.get('*', function (req, res) {
-  // res.sendFile(path.join(__dirname1, 'build', 'index.html'));
-  res.sendFile('index.html', { root: './build' })
+    res.sendFile('index.html', {
+        root: './build'
+    })
 });
 
 app.listen(port, function () {
-  console.log('GotPop is running on http://localhost:' + port);
+    console.log('GotPop is running on http://localhost:' + port);
 });
