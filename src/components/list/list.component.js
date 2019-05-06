6cc7838213545c2076cef03de5ab38b4feaa22b4
gotpop <liam@gotpop.net>
2019-05-06 23:28:47 +0100

    Quick svg edits

--- src/components/list/list.component.js
+++ src/components/list/list.component.js
@@ -6,8 +6,16 @@ class ListComponent extends Component {
     render() {
         return (
             <li>
-                <div>{this.props.item.name}</div>
-                <Icon name={this.props.item.name}/>
+                <article className="client">
+                    <h3>{this.props.item.name}</h3>
+                    <div className="logo__wrap">
+                        <Icon
+                            name={this.props.item.name}
+                            width="200"
+                            height="100"
+                            viewBox="0 0 200 50"/>
+                    </div>
+                </article>
             </li>
         );
     }
