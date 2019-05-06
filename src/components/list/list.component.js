81f7c79b0b4d3d511f11dc8a0b7fc64df4f48895
gotpop <liam@gotpop.net>
2019-05-06 21:58:55 +0100

    Tidy list

--- src/components/list/list.component.js
+++ src/components/list/list.component.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
 import "./list.component.scss";
-// import {ReactComponent as Logo} from '../../assets/img/svg/logos/domestos.svg';
 import Icon from '../Icons';
-
 
 class ListComponent extends Component {
     render() {
         return (
             <li>
                 <div>{this.props.item.name}</div>
-                <h1>Icon</h1>
                 <Icon name="Domestos"/>
             </li>
         );
