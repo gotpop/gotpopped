50ad802b68737067663200f712e6044ca1fa1fd8
gotpop <liam@gotpop.net>
2019-05-06 18:58:29 +0100

    Logo rendered

--- src/components/list/list.component.js
+++ src/components/list/list.component.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import "./list.component.scss";
+import {ReactComponent as Logo} from '../../assets/img/svg/logos/domestos.svg';
 
 class ListComponent extends Component {
     render() {
         return (
             <li>
                 <div>{this.props.item.name}</div>
+                <div className="logo__wrap"><Logo/></div>
             </li>
         );
     }
