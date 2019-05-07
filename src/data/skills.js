3c4e93aab9ed3402fffda0a7da8fa7ee05be753f
gotpop <liam@gotpop.net>
2019-05-07 23:56:20 +0100

    Updated icon styling and sizing

--- src/data/skills.js
+++ src/data/skills.js
@@ -3,50 +3,62 @@ const skillsObject = [
     {
         name: 'Angular',
         title: 'Domestos',
+        viewBox: '0 0 260 260',
         content: 'Some content'
     }, {
         name: 'Cssgrid',
         title: 'Mercedes Benz',
+        viewBox: '0 0 60 60',
         content: 'Some content'
     }, {
         name: 'Design',
         title: 'Aetna Insurance',
+        viewBox: '0 10 55 60',
         content: 'Some content'
     }, {
         name: 'Es6',
         title: 'Columbus Insurance',
+        viewBox: '0 0 260 260',
         content: 'Some content'
     }, {
         name: 'Git',
         title: 'Magnum Ice Cream',
+        viewBox: '0 0 100 100',
         content: 'Some content'
     }, {
         name: 'Html5',
         title: 'Virgin Radio',
+        viewBox: '0 0 500 600',
         content: 'Some content'
     }, {
         name: 'Jsdom',
         title: 'Discovery Education',
+        viewBox: '-149 451 100 100',
         content: 'Some content'
     }, {
-        name: 'React',
+        name: 'ReactIcon',
         title: 'Discovery Education',
+        viewBox: '0 0 800 800',
         content: 'Some content'
     }, {
         name: 'Redux',
         title: 'Discovery Education',
+        viewBox: '0 0 100 100',
         content: 'Some content'
     }, {
         name: 'Testing',
         title: 'Discovery Education',
+        viewBox: '0 0 70 70',
         content: 'Some content'
     }, {
         name: 'Vue',
         title: 'Discovery Education',
+        viewBox: '0 0 270 230',
         content: 'Some content'
     }, {
         name: 'Webpack',
         title: 'Discovery Education',
+        viewBox: '400 300 1000 1000',
         content: 'Some content'
     }
 ];
