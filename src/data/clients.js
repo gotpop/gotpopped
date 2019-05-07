1875e40ea11405407168986c6ac9320f5048ac6a
gotpop <liam@gotpop.net>
2019-05-07 10:55:38 +0100

    Formatting

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,12 +1,21 @@
-/* Skills data */
-const clientsObject = [
-  { name: 'Domestos' },
-  // { name: 'Mercedes Benz' },
-  { name: 'Aetna' },
-  { name: 'Columbus' },
-  { name: 'Magnum' },
-  // { name: 'Virgin Radio' }
-  { name: 'Discovery' }
+// Skills data 
+const clientsObject = [{
+        name: 'Domestos'
+    },
+    // { name: 'Mercedes Benz' },
+    {
+        name: 'Aetna'
+    },
+    {
+        name: 'Columbus'
+    },
+    {
+        name: 'Magnum'
+    },
+    // { name: 'Virgin Radio' }
+    {
+        name: 'Discovery'
+    }
 ];
 
 export default clientsObject;
