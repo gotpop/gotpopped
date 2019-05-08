5b114faa0bdcfbcbd90a99823c9776af1a61a237
gotpop <liam@gotpop.net>
2019-05-08 23:47:59 +0100

    Removed transition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
-import {AnimatedSwitch} from 'react-router-transition';
 import './App.scss';
 
 import Header from './components/Header/Header';
@@ -16,16 +15,10 @@ class App extends Component {
                 <div className="ap">
                     <Header/>
                     <section className="si">
-                        <AnimatedSwitch
-                            atEnter={{ opacity: 0 }}
-                            atLeave={{ opacity: 0 }}
-                            atActive={{ opacity: 1 }}
-                            className="switch-wrapper">
-                            <Route exact path="/" component={Home}/>
-                            <Route path="/clients" component={Clients}/>
-                            <Route path="/work" component={Work}/>
-                            <Route path="/skills" component={Skills}/>
-                        </AnimatedSwitch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/clients" component={Clients}/>
+                        <Route path="/work" component={Work}/>
+                        <Route path="/skills" component={Skills}/>
                     </section>
                 </div>
             </BrowserRouter>
