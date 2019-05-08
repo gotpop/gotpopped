223caefe24ace78076238ba853e32f0ebe08bef4
gotpop <liam@gotpop.net>
2019-05-08 11:40:19 +0100

    Added missing data

--- src/data/clients.js
+++ src/data/clients.js
@@ -3,30 +3,37 @@ const clientsObject = [
     {
         name: 'Domestos',
         title: 'Domestos',
+        viewBox: '0 0 260 260',
         content: 'Some content'
     }, {
         name: 'Mercedes',
         title: 'Mercedes Benz',
-        content: 'Some content'
+        viewBox: '0 0 260 260',
+        content: 'Some content about Mercedes'
     }, {
         name: 'Aetna',
         title: 'Aetna Insurance',
+        viewBox: '0 0 260 260',
         content: 'Some content'
     }, {
         name: 'Columbus',
         title: 'Columbus Insurance',
+        viewBox: '0 0 260 260',
         content: 'Some content'
     }, {
         name: 'Magnum',
         title: 'Magnum Ice Cream',
+        viewBox: '0 0 260 260',
         content: 'Some content'
     }, {
         name: 'Virgin',
         title: 'Virgin Radio',
+        viewBox: '0 0 260 260',
         content: 'Some content'
     }, {
         name: 'Discovery',
         title: 'Discovery Education',
+        viewBox: '0 0 260 260',
         content: 'Some content'
     }
 ];
