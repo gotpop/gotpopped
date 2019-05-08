9ad2b881a9e76875205e0a342f7072af0856cb22
gotpop <liam@gotpop.net>
2019-05-08 12:21:23 +0100

    Updated viewbox data

--- src/data/clients.js
+++ src/data/clients.js
@@ -3,37 +3,37 @@ const clientsObject = [
     {
         name: 'Domestos',
         title: 'Domestos',
-        viewBox: '0 0 260 260',
+        viewBox: '0 0 400 400',
         content: 'Some content'
     }, {
         name: 'Mercedes',
         title: 'Mercedes Benz',
-        viewBox: '0 0 260 260',
+        viewBox: '0 0 400 400',
         content: 'Some content about Mercedes'
     }, {
         name: 'Aetna',
         title: 'Aetna Insurance',
-        viewBox: '0 0 260 260',
+        viewBox: '0 0 400 400',
         content: 'Some content'
     }, {
         name: 'Columbus',
         title: 'Columbus Insurance',
-        viewBox: '0 0 260 260',
+        viewBox: '0 0 400 400',
         content: 'Some content'
     }, {
         name: 'Magnum',
         title: 'Magnum Ice Cream',
-        viewBox: '0 0 260 260',
+        viewBox: '0 0 400 400',
         content: 'Some content'
     }, {
         name: 'Virgin',
         title: 'Virgin Radio',
-        viewBox: '0 0 260 260',
+        viewBox: '0 0 400 400',
         content: 'Some content'
     }, {
         name: 'Discovery',
         title: 'Discovery Education',
-        viewBox: '0 0 260 260',
+        viewBox: '0 0 400 400',
         content: 'Some content'
     }
 ];
