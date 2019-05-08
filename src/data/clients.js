b4113637a0141dc1714ef036ec56813372a66a26
gotpop <liam@gotpop.net>
2019-05-08 18:56:07 +0100

    Modified data

--- src/data/clients.js
+++ src/data/clients.js
@@ -3,39 +3,41 @@ const clientsObject = [
     {
         name: 'Domestos',
         title: 'Domestos',
-        viewBox: '0 0 30 30',
+        viewBox: '0 0 300 300',
         content: 'Some content'
     }, {
         name: 'Mercedes',
         title: 'Mercedes Benz',
-        viewBox: '-20 0 250 60',
+        viewBox: '-20 0 280 70',
         content: 'Some content about Mercedes'
     }, {
         name: 'Aetna',
         title: 'Aetna Insurance',
-        viewBox: '50 0 150 80',
+        viewBox: '0 0 190 100',
         content: 'Some content'
     }, {
         name: 'Columbus',
         title: 'Columbus Insurance',
-        viewBox: '0 0 200 200',
+        viewBox: '90 0 400 350',
         content: 'Some content'
     }, {
         name: 'Magnum',
         title: 'Magnum Ice Cream',
-        viewBox: '40 0 190 100',
+        viewBox: '110 0 110 80',
         content: 'Some content'
     }, {
         name: 'Virgin',
         title: 'Virgin Radio',
-        viewBox: '10 10 800 800',
-        content: 'Some content'
-    }, {
-        name: 'Discovery',
-        title: 'Discovery Education',
-        viewBox: '0 0 50 40',
+        viewBox: '10 10 1000 1200',
         content: 'Some content'
     }
+    
+    // , {
+    //     name: 'Discovery',
+    //     title: 'Discovery Education',
+    //     viewBox: '0 0 300 200',
+    //     content: 'Some content'
+    // }
 ];
 
 export default clientsObject;
