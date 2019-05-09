ae50841dc83a4cbfefbe90aea007407fd9ad795d
gotpop <liam@gotpop.net>
2019-05-09 14:46:33 +0100

    Added dummy text

--- src/data/clients.js
+++ src/data/clients.js
@@ -4,32 +4,32 @@ const clientsObject = [
         name: 'unilever',
         title: 'Unilever',
         viewBox: '0 0 300 300',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'mercedes',
         title: 'Mercedes Benz',
         viewBox: '-20 0 280 70',
-        content: 'Some content about Mercedes'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'aetna',
         title: 'Aetna Insurance',
         viewBox: '0 0 190 100',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'columbus',
         title: 'Columbus Insurance',
         viewBox: '90 0 400 350',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'virgin',
         title: 'Virgin Radio',
         viewBox: '10 10 1000 1200',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'discovery',
         title: 'Discovery Education',
         viewBox: '0 0 300 200',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }
 ];
 
--- src/data/skills.js
+++ src/data/skills.js
@@ -4,62 +4,62 @@ const skillsObject = [
         name: 'angular',
         title: 'Angular',
         viewBox: '0 0 260 260',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'cssgrid',
         title: 'Css Grid',
         viewBox: '0 0 60 60',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'design',
         title: 'Design',
         viewBox: '0 10 55 60',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'es6',
         title: 'ES6',
         viewBox: '0 0 260 260',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'git',
         title: 'Git',
         viewBox: '0 0 100 100',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'html5',
         title: 'Html5',
         viewBox: '0 0 500 600',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'jsdom',
         title: 'Jsdom',
         viewBox: '-149 451 100 100',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'reactIcon',
         title: 'React',
         viewBox: '0 0 800 800',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'redux',
         title: 'Redux',
         viewBox: '0 0 100 100',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'testing',
         title: 'Testing',
         viewBox: '0 0 70 70',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'vue',
         title: 'VueJS',
         viewBox: '0 0 270 230',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'webpack',
         title: 'Webpack',
         viewBox: '400 300 1000 1000',
-        content: 'Some content'
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }
 ];
 
