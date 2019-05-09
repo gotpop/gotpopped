624bd0bd3393d3b017c86d7a31c4e421a364786f
gotpop <liam@gotpop.net>
2019-05-09 23:15:03 +0100

    Logo viewbox

--- src/data/clients.js
+++ src/data/clients.js
@@ -3,12 +3,12 @@ const clientsObject = [
     {
         name: 'unilever',
         title: 'Unilever',
-        viewBox: '0 0 300 300',
+        viewBox: '0 10 100 100',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'mercedes',
         title: 'Mercedes Benz',
-        viewBox: '-20 0 280 70',
+        viewBox: '0 0 80 80',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     }, {
         name: 'aetna',
