bbb6ed62abd96dca942a1044e5d9dad70d45a9fd
gotpop <liam@gotpop.net>
2019-05-13 14:30:13 +0100

    Updated skills content

--- src/data/skills.js
+++ src/data/skills.js
@@ -4,32 +4,32 @@ const skillsObject = [
         name: 'reactIcon',
         title: 'React',
         viewBox: '0 0 800 800',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        content: 'React is my "go to" library for building single page applications. I\'ve used for building component based apps at Audio Network'
     }, {
         name: 'cssgrid',
         title: 'Css Grid',
         viewBox: '0 0 60 60',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        content: 'CSS Grid Layout is the preffered method of building pixel perfect grid based layouts. I now use it to layout the majority of new projects.'
     }, {
         name: 'es6',
         title: 'ES6',
         viewBox: '0 0 260 260',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        content: 'Es6 is the shorthand for new Javascript standards that bring a whole host of new features such as Es6 modules.'
     }, {
         name: 'design',
         title: 'Design',
         viewBox: '0 10 55 60',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        content: 'Although I am now firmly a developer with over 7 years worth of experience, I (prehaps unusually) came from a design background before engtering into web development.'
     }, {
         name: 'git',
         title: 'Git',
         viewBox: '0 0 100 100',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        content: 'As is of course standard I use Git on every project and is naturally an invaluble tool for tracking and backing up your work.'
     }, {
         name: 'webpack',
         title: 'Webpack',
         viewBox: '400 300 1000 1000',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        content: 'I\'ve worked with Grunt, Gulp and more recently Parcel for bundling front end assets. However the majority of projects I work on use Webpack and more recently Webpack 4.'
     }, {
         name: 'jsdom',
         title: 'Jsdom',
