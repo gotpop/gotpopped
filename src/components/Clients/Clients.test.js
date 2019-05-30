14333b605d39917f8ae1b4ad3b6cb12dd0482079
gotpop <liam@gotpop.net>
2019-05-30 22:58:19 +0100

    Added basic tests

---
+++ src/components/Clients/Clients.test.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// Test info
+// import info from './info';
+
+describe('Clients', () => {
+
+  it('should init', () => {
+    // const thisButton = document.getElementById('selectInfoYears');
+    // const thisBlock = document.querySelector('.select__block');
+    // const goInfo = new info();
+
+    // goInfo.init();
+    // thisButton.focus();
+    // const checkClassOnBlock = thisBlock.classList.contains('select__block--active');
+
+    // expect(checkClassOnBlock).toBe(true);
+  });
+
+});
