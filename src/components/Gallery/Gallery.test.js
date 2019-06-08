a3d0702d993cd3e357dec95a4e87bd4796116edb
gotpop <liam@gotpop.net>
2019-06-08 19:28:35 +0100

    Added shallow tests

--- src/components/Gallery/Gallery.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Gallery from './Gallery';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Gallery />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/components/Header/header.test.js
+++ src/components/Header/header.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Work from './Work';
+import Header from './Header';
 
 configure({ adapter: new Adapter() });
 
-it('renders Work', () => {
-    const work = shallow(<Work />);
-    expect(work.find('main').length).toEqual(1);
+it('renders header', () => {
+    const header = shallow(<Header />);
+    expect(header.find('header').length).toEqual(1);
 });
